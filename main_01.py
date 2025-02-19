1d5f6cb6d2691ca554653af26cf6884a5270bcec
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-19 18:38:00 +0100

    Update main_01.py

--- main_01.py
+++ main_01.py
@@ -5,23 +5,21 @@ a podaci koji se čuvaju o svakom vozilu su:
 tip, proizvođač, registarska oznaka, godina prve registracije te cijena u eur.
 '''
 
-vehicle_type = '' # car, truck, motorcycle, bicycle, agriculture, marine
-manufacturer = ''
-production_year = 1990
-license_plate = ''
-purchased_price = 0.0
+# vehicle_type => car, truck, motorcycle, bicycle, agriculture, marine
+vehicle = {} # prazan set => set(), a prazan rjecnik samo s viticasatim zagradama {}
 
 vehicle_db = []
 
 while True:
     # TODO Ispisite sve tipove vozila tako da ispred svakog navedete redni broj
     #   Trazite korisnika da selektira jedan broj i tako izabere tip vozila
-    vehicle_type = input('Upisite tip vozila: ')
-    manufacturer = input('Upisite proizvodaca vozila: ')
-    production_year = int(input('Upisite godinu proizvodnje vozila: '))
-    license_plate = input('Upisite registarsku oznaku vozila: ')
-    purchased_price = float(input('Upisite nabavnu cijenu vozila: '))
+    vehicle['vehicle_type'] = input('Upisite tip vozila: ')
+    vehicle['manufacturer'] = input('Upisite proizvodaca vozila: ')
+    vehicle['production_year'] = int(input('Upisite godinu proizvodnje vozila: '))
+    vehicle['license_plate'] = input('Upisite registarsku oznaku vozila: ')
+    vehicle['purchased_price'] = float(input('Upisite nabavnu cijenu vozila: '))
 
+    vehicle_db.append(vehicle)
 
     next_vehicle = input('Zelite li dodati novo vozilo? (da/ne): ')
     if next_vehicle.lower() != 'da':
